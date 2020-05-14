f4fdd703d2f17556e5d1dba16cbe43a5bbc7fde4
Abhishek Kumar Gupta <abhishekguptaatweb17@gmail.com>
2020-05-15 00:23:42 +0530

    make all responsive

--- functions.js
+++ functions.js
@@ -129,3 +129,105 @@ function setDuration()
   	savings_amount.innerHTML = icecandy*x[1].innerHTML-lollipop*x[0].innerHTML; 
 }
 
+var i=0;
+var images = [];
+var time = 1500;
+var ovals = document.getElementsByClassName("sample-oval");
+var opinions = document.getElementsByClassName("customer-quotes");
+var names = document.getElementsByClassName("name");
+var locations = document.getElementsByClassName("location");
+
+var customer_names = ["Raja Ram", "Kane", "Abhishek", "Fyle"];
+
+var comments = ["Lorem Ipsum is simply dummy text of the<br> &emsp;&nbsp;printing and typesetting industry. "
+ +"Lorem Ipsum<br>&emsp; has been the industry's standard dummy text<br>&emsp; ever since the 1500s.","This is just an opinion.","This is another opinion.<br>"
+ +"&emsp; Didn't Knew will learn so much from <br>&emsp; building a simple Website.",
+"Don't Know about it."];
+
+var customer_locations = ["Tip World", "Delhi", "Bihar", "Karnataka"];
+
+images[0] = "./assets/images/profile.png";
+images[1] = "./assets/images/profile2.png";
+images[2] = "./assets/images/profile3.png";
+images[3] = "./assets/images/profile4.png";
+
+function changeCustomers()
+{
+  document.slide.src = images[i];
+
+  ovals[i].classList.add("oval-active"); 
+  ovals[i].classList.remove("oval-dead");
+  
+  opinions[0].innerHTML = comments[i];
+  names[0].innerHTML = customer_names[i];
+  locations[0].innerHTML = customer_locations[i];
+
+    if(i>0)   
+    ovals[i-1].classList.add("oval-dead");  
+    if(i==0)
+     ovals[ovals.length-1].classList.add("oval-dead"); 
+  
+  if(i < images.length - 1)
+  {
+    i++;
+  }
+  else
+    i=0;
+// if(ovals[i].classList.contains("oval-active"))
+}
+
+setInterval("changeCustomers()" , time);
+
+ovals[0].onclick = changeCustomers0;
+ovals[1].onclick = changeCustomers1;
+ovals[2].onclick = changeCustomers2;
+ovals[3].onclick = changeCustomers3;
+
+
+function changeCustomers0()
+{
+   document.slide.src = images[0];
+
+  ovals[0].classList.add("oval-active"); 
+  ovals[0].classList.remove("oval-dead");
+  
+  opinions[0].innerHTML = comments[0];
+  names[0].innerHTML = customer_names[0];
+  locations[0].innerHTML = customer_locations[0];
+}
+
+function changeCustomers1()
+{
+   document.slide.src = images[1];
+
+  ovals[1].classList.add("oval-active"); 
+  ovals[1].classList.remove("oval-dead");
+  
+  opinions[0].innerHTML = comments[1];
+  names[0].innerHTML = customer_names[1];
+  locations[0].innerHTML = customer_locations[1];
+}
+
+function changeCustomers2()
+{
+   document.slide.src = images[2];
+
+  ovals[2].classList.add("oval-active"); 
+  ovals[2].classList.remove("oval-dead");
+  
+  opinions[0].innerHTML = comments[2];
+  names[0].innerHTML = customer_names[2];
+  locations[0].innerHTML = customer_locations[2];
+}
+
+function changeCustomers3()
+{
+   document.slide.src = images[3];
+
+  ovals[3].classList.add("oval-active"); 
+  ovals[3].classList.remove("oval-dead");
+  
+  opinions[0].innerHTML = comments[3];
+  names[0].innerHTML = customer_names[3];
+  locations[0].innerHTML = customer_locations[3];
+}
